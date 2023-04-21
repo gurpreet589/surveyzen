1e800082e893aacb1682ded5eb06efa35fcbef9d
Gurpreet Singh Bhuee <gurpreet_singh_bhuee@epam.com>
2023-04-21 10:36:40 +0100

    Add files via upload

---
+++ surveyzen/src/main/java/com/reply/hackathon/surveyzen/SurveyzenApplication.java
@@ -0,0 +1,13 @@
+package com.reply.hackathon.surveyzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyzenApplication.class, args);
+	}
+
+}
---
+++ surveyzen/src/test/java/com/reply/hackathon/surveyzen/SurveyzenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.hackathon.surveyzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
